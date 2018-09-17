165049e4363525f84b5cf8cda85e06be63526281
Jeffrey Lou <jeffzlou@gmail.com>
2018-09-16 22:36:54 -0400

    forgot comma

--- radon-station.py
+++ radon-station.py
@@ -52,7 +52,7 @@ def get_data():
             value = struct.unpack(s.format_type, value)
             data[s.name] = value
     p.disconnect()
-    logging.info('data received:' data)
+    logging.info('data received:', data)
     return data
 
 
